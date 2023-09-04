f3d9a6c3ae7330ecf23730f734baf1f65a5bc7bd
CNife <CNife@vip.qq.com>
2023-09-04 11:26:49 +0800

    更新test_all.py

--- python/test_all.py
+++ python/test_all.py
@@ -1,21 +1,19 @@
-import itertools
 import logging
 import os
 import subprocess
-from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 
 logger = logging.getLogger()
 
-DEFAULT_PYTHON: str = os.environ.get("DEFAULT_PYTHON", os.path.join(__file__, "../.venv/Scripts/python.exe"))
-DEFAULT_CODE_DIR: str = os.environ.get("DEFAULT_CODE_DIR", os.path.join(__file__, "../src/code"))
-DEFAULT_APPEND_PYTHON_PATH: str = os.environ.get("DEFAULT_APPEND_PYTHON_PATH", os.path.join(__file__, "../src"))
-
 
 def main() -> None:
-    code_dir = Path(DEFAULT_CODE_DIR).resolve()
-    append_python_path = os.pathsep.join(os.path.abspath(p) for p in DEFAULT_APPEND_PYTHON_PATH.split(","))
-    logger.info(f"python_exe={DEFAULT_PYTHON}")
+    python_exe = Path(os.environ.get("PYTHON_EXE", os.path.join(__file__, "../.venv/Scripts/python.exe"))).resolve()
+    code_dir = Path(os.environ.get("DEFAULT_CODE_DIR", os.path.join(__file__, "../src/code"))).resolve()
+    append_python_path = os.pathsep.join(
+        os.path.abspath(p)
+        for p in os.environ.get("DEFAULT_APPEND_PYTHON_PATH", os.path.join(__file__, "../src")).split(",")
+    )
+    logger.info(f"{python_exe=}")
     logger.info(f"code_dir={str(code_dir)}")
     logger.info(f"{append_python_path=}")
 
@@ -26,26 +24,17 @@ def main() -> None:
         os.environ[python_path] = append_python_path
     logger.info(f"PYTHONPATH={os.environ[python_path]}")
 
-    with ThreadPoolExecutor() as executor:
-        result = list(
-            executor.map(
-                run_solution,
-                itertools.repeat(DEFAULT_PYTHON),
-                [solution for solution in code_dir.glob("*.py") if solution.is_file()],
-            )
-        )
-    success_count = sum(result)
-    logger.info(f"{success_count} success, {len(result) - success_count} failed in {len(result)} solutions")
-
-
-def run_solution(python_exe: str, solution: Path) -> bool:
-    process = subprocess.run([str(python_exe), str(solution)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    if process.returncode == 0:
-        logger.info(f"SUCCESS {solution.stem}")
-    else:
-        logger.error(f"FAIL {solution.stem}")
-        logger.error(process.stdout)
-    return process.returncode == 0
+    for solution in code_dir.glob("*.py"):
+        if solution.is_file():
+            try:
+                subprocess.run(
+                    [str(python_exe), str(solution)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True
+                )
+                logger.info(f"SUCCESS {solution.stem}")
+            except subprocess.CalledProcessError as e:
+                logger.error(f"FAILED {solution.stem}")
+                raise e
+    logger.info("ALL DONE")
 
 
 if __name__ == "__main__":
